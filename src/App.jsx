c8cd3f75f49af89ce542d136028ba14ca76e1aca
Victor Hernandez <hernandezv0110@gmail.com>
2024-11-02 21:53:38 -0700

    Added h1 title and listed 3 items

--- src/App.jsx
+++ src/App.jsx
@@ -3,31 +3,34 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+const toDoList = [
+  {
+    id: 1,
+    title: 'Complete Lesson 1.1',
+  },
+  {
+    id: 2,
+    title: 'Go to the gym',
+  },
+  {
+    id: 3,
+    title: 'Meal prep for the week',
+  },
+];
 
+function App() {
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <h1>
+          Todo List
+        </h1>
+        <ul>
+          {toDoList.map(function (item){
+            return <li key={item.id}>{item.title}</li>;
+          })}
+        </ul>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
