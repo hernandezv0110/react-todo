ae16c2c5a4ab4531fa3397607e7351442c20a6e5
Victor Hernandez <hernandezv0110@gmail.com>
2025-01-14 08:07:39 -0800

    Setting and getting from local storage works

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,20 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./App.css";
 import TodoList from "./TodoList";
 import AddToDoForm from "./AddTodoForm";
 
 function App() {
-  const [todoList, setTodoList] = useState([]);
+  const [todoList, setTodoList] = useState(
+    JSON.parse(localStorage.getItem("savedTodoList")) || []
+  );
 
   const addTodo = (newTodo) => {
     setTodoList([...todoList, newTodo]);
   };
+
+  useEffect(() => {
+    localStorage.setItem("savedTodoList", JSON.stringify(todoList));
+  }, [todoList]);
 
   return (
     <div>
