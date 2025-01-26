614920b49fedf747a23bdd3d960afd8610cd2973
Victor Hernandez <hernandezv0110@gmail.com>
2025-01-25 20:48:46 -0800

    Completed asynchronous data

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,31 @@ import TodoList from "./TodoList";
 import AddToDoForm from "./AddTodoForm";
 
 function App() {
-  const [todoList, setTodoList] = useState(
-    () => JSON.parse(localStorage.getItem("savedTodoList")) || []
-  );
+  // const [todoList, setTodoList] = useState(
+  //   () => JSON.parse(localStorage.getItem("savedTodoList")) || []
+  // );
+  const [todoList, setTodoList] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
-    localStorage.setItem("savedTodoList", JSON.stringify(todoList));
+    new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve({
+          data: {
+            todoList: JSON.parse(localStorage.getItem("savedTodoList")) || [],
+          },
+        });
+      }, 2000);
+    }).then((result) => {
+      setTodoList(result.data.todoList);
+      setIsLoading(false);
+    });
+  }, []);
+
+  useEffect(() => {
+    if (!isLoading) {
+      localStorage.setItem("savedTodoList", JSON.stringify(todoList));
+    }
   }, [todoList]);
 
   const addTodo = (newTodo) => {
@@ -26,7 +45,11 @@ function App() {
     <Fragment>
       <h1>Todo List</h1>
       <AddToDoForm onAddTodo={addTodo} />
-      <TodoList todoList={todoList} onRemoveTodo={removeTodo} />
+      {isLoading ? (
+        <p>Loading...</p>
+      ) : (
+        <TodoList todoList={todoList} onRemoveTodo={removeTodo} />
+      )}
     </Fragment>
   );
 }
